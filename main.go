39cbc2cb3e0ae5d614128235c26ec72f2bb87a8c
Aaron Salvo <aaron.salvo@teamaol.com>
2016-10-24 12:14:22 -0400

    Formatted.

--- main.go
+++ main.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "errors"
-  "log"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
 )
 
 type capitalized interface {
-  Capitalize() string
+	Capitalize() string
 }
 
 type saidName string
 
 func (n saidName) Capitalize() string {
-  return strings.Title(string(n))
+	return strings.Title(string(n))
 }
 
 type yelledName string
 
 func (n yelledName) Capitalize() string {
-  return strings.ToUpper(string(n))
+	return strings.ToUpper(string(n))
 }
 
 func main() {
-  dilbertSays := saidName("aaron")
-  howardSays := yelledName(dilbertSays)
+	dilbertSays := saidName("aaron")
+	howardSays := yelledName(dilbertSays)
 
-  speakers := []capitalized{dilbertSays, howardSays}
+	speakers := []capitalized{dilbertSays, howardSays}
 
-  for _, speaker := range speakers {
-    say(speaker)
-  }
+	for _, speaker := range speakers {
+		say(speaker)
+	}
 
 }
 
 func say(speaker interface{}) {
-  var whoSays, greeting, name string
+	var whoSays, greeting, name string
 
-  switch val := speaker.(type) {
-  case saidName:
-    whoSays = "Dilbert"
-    greeting = "Hello"
-    name = val.Capitalize()
-  case yelledName:
-    whoSays = "Loud Howard"
-    greeting = "HELLO"
-    name = val.Capitalize()
-  default:
-    log.Fatal(errors.New("Unknown type"))
-  }
+	switch val := speaker.(type) {
+	case saidName:
+		whoSays = "Dilbert"
+		greeting = "Hello"
+		name = val.Capitalize()
+	case yelledName:
+		whoSays = "Loud Howard"
+		greeting = "HELLO"
+		name = val.Capitalize()
+	default:
+		log.Fatal(errors.New("Unknown type"))
+	}
 
-  fmt.Printf("%s says, '%s, %s'\n", whoSays, greeting, name)
+	fmt.Printf("%s says, '%s, %s'\n", whoSays, greeting, name)
 }
